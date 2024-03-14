9442d29c7934486597a856815e96d6b66e32b830
bonugg1 <159542433+bonugg1@users.noreply.github.com>
2024-03-14 14:50:14 +0000

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -14,7 +14,7 @@ const BADGES = [
 ];
 
 const USER_ID = "986307077611847711"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
-const BACKGROUND_IMAGE = "https://j.gifs.com/vMeoOl.gif"; // Here is the background of the site, behind your profile
+const BACKGROUND_IMAGE = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2FHotlineMiami%2Fcomments%2Fkk57v4%2Fanimated_versions_of_the_wallpaperpfp_that_i%2F&psig=AOvVaw20X0rVl5CLxXzOzH2f8cq2&ust=1710513259046000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNic4Mr884QDFQAAAAAdAAAAABAZ"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
     name: "Do not disturb", // Discord Status
